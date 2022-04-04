24091488e24500c09b181776c21a30681aebfcaf
DaSwow <carls09995@hotmail.com>
2022-04-04 02:53:24 -0700

    Push
    Carcasa

---
+++ ClienteDoctor/src/cliente/Client.java
@@ -0,0 +1,107 @@
+package cliente;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+
+    private Socket socket;
+
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private Boolean autorizacion;
+
+    public Client() {
+        try {
+            if (socket == null) {
+                socket = new Socket("localhost", 1234);
+                oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.flush();
+                ois = new ObjectInputStream(socket.getInputStream());
+                listenForMessage();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, ois, oos);
+        }
+    }
+
+    public void sendMessage(String asunto, ArrayList mensaje) {
+        try {
+            ArrayList msg = new ArrayList();
+            msg.add(asunto);
+            msg.add(mensaje);
+            oos.writeObject(msg);
+            oos.flush();
+        } catch (IOException e) {
+            closeEverything(socket, ois, oos);
+        }
+    }
+
+    public Boolean getAutorizacion() {
+        while (true) {
+            if (autorizacion != null) {
+                System.out.println("Autorizacion pedida: " + autorizacion);
+                return autorizacion;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void listenForMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (socket.isConnected()) {
+                    try {
+                        ArrayList msgRecibido = (ArrayList) ois.readObject();
+                        System.out.println(msgRecibido);
+                        String asunto = (String) msgRecibido.get(0);
+                        ArrayList msgEnviar = new ArrayList();
+                        switch (asunto) {
+                            case "Credenciales":
+                                msgEnviar.add(true);
+                                sendMessage("Credenciales", msgEnviar);
+                                Thread.sleep(1000);
+                                break;
+                            case "Respuesta Credenciales":
+                                autorizacion = (Boolean) msgRecibido.get(1);
+                                break;
+                            default:
+                                break;
+                        }
+                    } catch (IOException e) {
+                        closeEverything(socket, ois, oos);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (Exception exx) {
+                        exx.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, ObjectInputStream ois, ObjectOutputStream oos) {
+        try {
+            if (ois != null) {
+                ois.close();
+            }
+            if (oos != null) {
+                oos.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ ServidorSalud/src/servidor/Server.java
@@ -0,0 +1,125 @@
+package servidor;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ClientHandler implements Runnable {
+
+        private Socket socket;
+        private ObjectInputStream ois;
+        private ObjectOutputStream oos;
+        //Credenciales cliente
+        //private String clientUsername;
+
+        public ClientHandler(Socket socket) {
+            try {
+                this.socket = socket;
+                oos = new ObjectOutputStream(this.socket.getOutputStream());
+                oos.flush();
+                ois = new ObjectInputStream(this.socket.getInputStream());
+                System.out.println("Cliente Conectado");
+            } catch (IOException e) {
+                // Close everything more gracefully.
+                closeEverything(this.socket, ois, oos);
+            }
+        }
+
+        //Escucha mensajes permanentemente
+        @Override
+        public void run() {
+            while (socket.isConnected()) {
+                try {
+                    ArrayList messageFromClient = (ArrayList) ois.readObject();
+                    System.out.println(messageFromClient);
+                    String asunto = (String) messageFromClient.get(0);
+                    switch (asunto) {
+                        case "Credenciales":
+                            ArrayList respuesta = new ArrayList();
+                            respuesta.add("Respuesta Credenciales");
+                            respuesta.add(true);
+                            broadcastMessage(respuesta);
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    closeEverything(socket, ois, oos);
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (Exception exx) {
+                    exx.printStackTrace();
+                }
+            }
+        }
+
+        public void broadcastMessage(ArrayList messageToSend) throws IOException {
+            try {
+                oos.writeObject(messageToSend);
+                oos.flush();
+            } catch (IOException e) {
+                closeEverything(socket, ois, oos);
+            }
+
+        }
+
+        public void closeEverything(Socket socket, ObjectInputStream ois, ObjectOutputStream oos) {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+                if (oos != null) {
+                    oos.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
