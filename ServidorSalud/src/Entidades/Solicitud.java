0e54da0f40f01d725dabf355f25bc26516c4df82
DaSwow <carls09995@hotmail.com>
2022-05-17 19:48:55 -0700

    asd

--- ServidorSalud/src/Entidades/Solicitud.java
+++ ServidorSalud/src/Entidades/Solicitud.java
@@ -55,13 +55,6 @@ public class Solicitud implements Serializable {
         this.idsolicitud = idsolicitud;
     }
 
-    public Solicitud(Integer iddoctor, Integer idpaciente, String nsspaciente, Short aceptado) {
-        this.iddoctor = iddoctor;
-        this.idpaciente = idpaciente;
-        this.nsspaciente = nsspaciente;
-        this.aceptado = aceptado;
-    }
-
     public Integer getIdsolicitud() {
         return idsolicitud;
     }
